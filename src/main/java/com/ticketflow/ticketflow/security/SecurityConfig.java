28a828633db4b3816ab8a145e46dc309474b4e24
Rasim <rasimofficial06@gmail.com>
2026-08-28 16:16:52 +0530

    allow production domain in CORS config

--- src/main/java/com/ticketflow/ticketflow/security/SecurityConfig.java
+++ src/main/java/com/ticketflow/ticketflow/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "https://tickflow-issm.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
 
